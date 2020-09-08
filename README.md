52d0f2493c612034003b4ef9b6ae19c98792c4a6
Vincent Iroleh <irolehiroleh@gmail.com>
2020-09-08 14:46:54 +0100

    Making updates and upgrade easier

---
+++ README.md
@@ -0,0 +1,9 @@
+# Linux (deb) Update and Upgrade script
+
+An automated script that updates and upgrade your linux kernel and remove unneeded packages.
+
+## Installation
+
+- `cd udug` after downloading
+- `chmod u=rwx udug.sh` - Give the u (owner) rwx(read, write, execute) permissions
+- `./udug.sh` - To execute the program
---
+++ udug.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+############################
+#
+# Linux (deb) Update and Upgrade script.
+#
+############################
+chmod u=rwx udug.sh
+hostname=$(hostname -s)
+
+# Print start status message.
+echo "Updating and Upgrading $hostname"
+date
+echo
+# Updating and upgrading
+sudo apt-get -y update && sudo apt-get -y upgrade
+echo "Removing packages that are no longer needed from $hostname"
+sudo apt-get -y autoremove
+# Print end status message.
+echo
+echo "Update and Upgrade finished"
+date
